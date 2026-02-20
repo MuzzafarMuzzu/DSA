152002f72beb64f87076085e4a7549126349431a
MuzzafarMuzzu <muzzafarmujju10@gmail.com>
2026-02-20 11:58:21 +0530

    Update Method.java

--- Method.java
+++ Method.java
@@ -1,14 +1,1 @@
-public class Method {
-    public static void main(String[] args) {
-        String luffy = display();
-        System.out.println(luffy);
-    }
-    static String display()
-    {
-        System.out.println("Hello World!!");
-        String demo = "One Piece is real";
-        return demo;
-        
-    }
-    
-}
+000
